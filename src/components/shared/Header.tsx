f62b3c9883d6d16ef51695878c7ca92e78df09ae
raza-1234 <ahmedraza.algolix@gmail.com>
2024-01-20 15:26:51 -0800

    create responsive header and footer design

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState } from 'react'
 import logo from "../../assets/logo.png"
 import { Link } from 'react-router-dom'
 import "../../css/Header.css"
@@ -8,6 +8,7 @@ import { Avatar, Badge } from 'antd';
 import { Icon } from '@iconify/react';
 import { CartList } from '../../types/types'
 import { FaAngleDown } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 type ParentProp = {
   cartList: CartList[]
@@ -16,6 +17,7 @@ type ParentProp = {
 const Header = ({cartList}: ParentProp) => {
   const { auth, setAuth }:any = useAuth();  
   const [showDropDown, setShowDropDown] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
   
   const LogOut = (): void => {
     logOut();
@@ -26,17 +28,19 @@ const Header = ({cartList}: ParentProp) => {
   return (
     <header className='bloowatch-header__wrapper'>
       <div className='bloowatch-header__content'>
-        <img src={logo} alt='logo'/>
+        <div className='bloowatch-header__logo'>
+          <img src={logo} alt='logo'/>
+        </div>
         <div className='bloowatch-header__nav-list'>
           <li>
-            <Link className = "bloowatch-header__nav-list-item" to = "/shop">Shop</Link>
+            <Link className = "bloowatch-header__nav-list-item" to = "/shop">Home</Link>
           </li>          
           { auth?.userEmail ? 
             <React.Fragment>
               <li className='bloowatch-header__cart-item'>
                 <Link className = "bloowatch-header__nav-list-item" to = "/cart">
                   <Badge showZero count = {cartList.length}>
-                    <Avatar style={{backgroundColor: 'transparent', border: "1px solid black" }} shape="square" size={35}>
+                    <Avatar style={{backgroundColor: 'transparent', border: "1px solid black" }} shape="square" size={30}>
                       <h4>
                         <Icon icon="ant-design:shopping-cart-outlined" />
                         Cart
@@ -73,6 +77,53 @@ const Header = ({cartList}: ParentProp) => {
               </li>
             </React.Fragment>
           }
+        </div>
+        {
+          mobileMenu &&
+          <div className='bloowatch-header__mobile-menu'>
+            <li>
+              <Link className = "bloowatch-header__nav-list-item" to = "/shop">Home</Link>
+            </li>          
+            { auth?.userEmail ? 
+              <React.Fragment>
+                <li className='bloowatch-header__cart-item'>
+                  <Link className = "bloowatch-header__nav-list-item" to = "/cart">
+                    Cart
+                  </Link>
+                </li>
+                <li className='bloowatch-header__dropdown'>
+                  <div className='bloowatch-header__dropdown-name'  onClick={() => setShowDropDown(!showDropDown)}>
+                    {auth?.userName}
+                    <FaAngleDown />
+                  </div>
+                  {
+                    showDropDown &&
+                    <div className="bloowatch-header__dropdown-content">
+                      <Link to = "/edit-user" onClick={() => setShowDropDown(!showDropDown)}>Edit Profile</Link>
+                      <Link to = "/" onClick={LogOut}>Log Out</Link>
+                    </div>
+                  }
+                </li>
+                {
+                  showDropDown &&
+                  <div onClick={() => setShowDropDown(!showDropDown)} className='bloowatch-screen__wrapper'/>
+                }
+              </React.Fragment>
+              :
+              <React.Fragment>
+                <li>
+                  <Link className = "bloowatch-header__nav-list-item" to = "/register">Register</Link>
+                </li>
+                <li>
+                  <Link className = "bloowatch-header__nav-list-item" to = "/">Login</Link>
+                </li>
+              </React.Fragment>
+            }
+          </div>
+        }
+
+        <div className='bloowatch-header__mobile-view'>
+          <GiHamburgerMenu onClick={() => setMobileMenu(!mobileMenu)}/>
         </div>
       </div>
     </header>
